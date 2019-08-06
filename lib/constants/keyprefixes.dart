2c08fb3253c2d5084de74d70eb9549bf28e6dc62
Anak Mirasing <thaihooligan@hotmail.com>
2019-08-06 12:44:53 +0700

    Added key prefixes string constant

---
+++ lib/constants/keyprefixes.dart
@@ -0,0 +1,6 @@
+class KeyPrefixes {
+  static const DerivedKey = "LIBRA WALLET: derived key\$";
+  static const LibraHashSuffix = "@@\$\$LIBRA\$\$@@";
+  static const MasterKeySalt = "LIBRA WALLET: master key salt\$";
+  static const MnemonicSalt = "LIBRA WALLET: mnemonic salt prefix\$";
+}
