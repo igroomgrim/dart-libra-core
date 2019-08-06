99b473ab1782eb4ddf8e7d88b8a7a0d427a34b36
Anak Mirasing <thaihooligan@hotmail.com>
2019-08-06 19:14:30 +0700

    Added helper function for Uint8List from String

---
+++ lib/utils/helper.dart
@@ -0,0 +1,9 @@
+import 'dart:typed_data';
+
+Uint8List createUint8ListFromString(String s) {
+  var ret = new Uint8List(s.length);
+  for (var i = 0; i < s.length; i++) {
+    ret[i] = s.codeUnitAt(i);
+  }
+  return ret;
+}
